1c8f403a4bb84300f657c8b128ea3ab48b96a564
Danish <dan71ish@gmail.com>
2024-05-24 19:11:48 +0530

    set up the folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,20 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [totalSpent, setTotalSpent] = useState(0);
+
+  useEffect(() => {
+    async function fetchTotal() {
+      const res = await fetch("/api/expenses/total-spent");
+      const data = await res.json();
+      setTotalSpent(data.total);
+    }
+    fetchTotal();
+  }, []);
+
   return (
     <>
       <Card className="w-[350px] mx-auto">
--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expenseRoute } from "./routes/expenses";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
-app.use("*", logger());
-app.route("/api/expense", expenseRoute);
-app.get("/", (c) => c.text("Hono!"));
 
-app.get("/test", (c) => c.json("Hello test"));
+app.use("*", logger());
+app.route("/api/expenses", expenseRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
