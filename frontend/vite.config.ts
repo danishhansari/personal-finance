1714063f14588f1d9f60ea834222ec5eda4bdff1
Danish <dan71ish@gmail.com>
2024-05-24 20:58:42 +0530

    reverting to 1c8f403a4bb84300f657c8b128ea3ab48b96a564

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_SERVER,
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expenseRoute } from "./routes/expenses";
-import { cors } from "hono/cors";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
-app.use("*", cors());
 
 app.use("*", logger());
 app.route("/api/expenses", expenseRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
