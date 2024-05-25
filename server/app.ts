4b376515ed1482bed0485324ceb89cbe4a78cec3
Danish <dan71ish@gmail.com>
2024-05-25 17:29:16 +0530

    working on kinde

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,16 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expenseRoute } from "./routes/expenses";
+import { authRoute } from "./routes/auth";
 import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use("*", logger());
-const apiRoutes = app.basePath("/api").route("/expenses", expenseRoute);
+const apiRoutes = app
+  .basePath("/api")
+  .route("/expenses", expenseRoute)
+  .route("/authRoute", authRoute);
 
 app.get("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
