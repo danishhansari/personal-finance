900eee1bd803810ca5f8b213e67f4bde0498956b
Danish <dan71ish@gmail.com>
2024-05-25 15:57:56 +0530

    routes done and working on post route from frontend

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { createFileRoute } from "@tanstack/react-router";
 import {
   Card,
   CardContent,
@@ -6,8 +7,11 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
+import { api } from "../lib/api";
 
-import { api } from "./lib/api";
+export const Route = createFileRoute("/")({
+  component: Index,
+});
 
 async function getTotalSpent() {
   const res = await api.expenses["total-spent"].$get();
@@ -18,7 +22,7 @@ async function getTotalSpent() {
   return data;
 }
 
-function App() {
+function Index() {
   const { isPending, error, data } = useQuery({
     queryKey: ["get-total-spent"],
     queryFn: getTotalSpent,
@@ -38,5 +42,3 @@ function App() {
     </>
   );
 }
-
-export default App;
